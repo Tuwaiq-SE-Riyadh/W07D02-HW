e74d91bbe144cc55b080da69e025befa1b9b5127
S-MW <72529306+S-MW@users.noreply.github.com>
2021-11-15 15:25:03 +0300

    Solve the vowels task

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,37 @@
+
 package com.company;
+
+import java.util.Arrays;
+
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args)
+    {
+        // Vowels in the word
+        String name = "Saad";
+        System.out.println("The Vowels in ["+name+"] are: "+countVowels(name));
+
+        // Max
+
+    }
+
+    private static int countVowels(String name)
+    {
+        char [] vowels = {'A','a','E','e','I','i','O','o','U','u'};
+        int count = 0;
+
+        for (int i = 0; i < name.length(); i++)
+        {
+            for (int j = 0; j < vowels.length; j++)
+            {
+                if(name.charAt(i) == vowels[j])
+                {
+                    count ++;
+                    break;
+                }
+            }
+        }
+        return count;
     }
 }
